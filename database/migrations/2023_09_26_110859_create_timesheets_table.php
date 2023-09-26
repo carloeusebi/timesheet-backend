77690c3b81bcc9db8f5290fbd6acacedd7e501ba
carloeusebi <carloeusebi@gmail.com>
2023-09-26 13:24:46 +0200

    first commit

---
+++ database/migrations/2023_09_26_110859_create_timesheets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('timesheets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('project_id')->nullOnDelete()->nullable();
+            $table->foreignId('activity_id')->nullOnDelete()->nullable();
+            $table->date('date');
+            $table->unsignedTinyInteger('hours');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('timesheets');
+    }
+};
